d7d08d6095304c93cf11a8ba5ccca0cc7c7a361b
Andy Hayward <ach@buteo.org>
2026-06-12 15:46:20 -0600

    Implement command handling and user login functionality

--- main.go
+++ main.go
@@ -1,26 +1,76 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 
 	"github.com/ach1000/gator/internal/config"
 )
 
+type state struct {
+	config *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	handler, ok := c.handlers[cmd.name]
+	if !ok {
+		return fmt.Errorf("unknown command: %s", cmd.name)
+	}
+
+	return handler(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return errors.New("username is required")
+	}
+
+	username := cmd.args[0]
+	if err := s.config.SetUser(username); err != nil {
+		return err
+	}
+
+	fmt.Printf("User set to %s\n", username)
+	return nil
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	err = cfg.SetUser("andy")
-	if err != nil {
-		panic(err)
+	s := &state{config: &cfg}
+	cmds := &commands{handlers: map[string]func(*state, command) error{}}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arguments provided")
+		os.Exit(1)
 	}
 
-	updatedCfg, err := config.Read()
-	if err != nil {
-		panic(err)
+	cmd := command{
+		name: os.Args[1],
+		args: os.Args[2:],
 	}
 
-	fmt.Printf("%+v\n", updatedCfg)
+	if err := cmds.run(s, cmd); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
